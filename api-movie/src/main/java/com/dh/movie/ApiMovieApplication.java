ded060d2e49333ac7d925f6b8a669b8163a964cf
Elias Vasquez <gamergy83@gmail.com>
2023-05-26 00:58:00 -0500

    adding configuration for load balancer and enable eurea client for micro services catalog and movie

--- api-movie/src/main/java/com/dh/movie/ApiMovieApplication.java
+++ api-movie/src/main/java/com/dh/movie/ApiMovieApplication.java
@@ -5,10 +5,11 @@ import com.dh.movie.repository.MovieRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-
+@EnableEurekaClient
 @SpringBootApplication
 @EnableJpaRepositories
 public class ApiMovieApplication {
